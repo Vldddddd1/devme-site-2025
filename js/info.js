d702a76aecc67aa3a464ac68f5ede1a002c01eda
Constantin Dumitru <dumitrubutco11@gmail.com>
2025-04-28 14:03:47 +0300

    revamped

--- js/info.js
+++ js/info.js
@@ -81,27 +81,6 @@ document.addEventListener('scroll', () => {
 }, false);
 
 /* ===================== HOW TO HACK YOUR SUMMER ===================== */
-document.addEventListener('scroll', () => {
-    var x = document.getElementById("howTo"); // containerul
-
-    var step1 = document.getElementById("step1");
-    var step2 = document.getElementById("step2");
-    var step3 = document.getElementById("step3");
-
-    if (window.pageYOffset >= x.offsetTop - 150)
-    {
-        step1.style.transform = "translateX(0)";
-    }
-    if (window.pageYOffset >= x.offsetTop)
-    {
-        step2.style.transform = "translateX(0)";
-    }
-    if (window.pageYOffset >= x.offsetTop + 200)
-    {
-        step3.style.transform = "translateX(0)";
-    }
-}, false);
-
 var executed4 = 0;
 document.addEventListener('scroll', () => {
     var x = document.getElementById("video-title");
